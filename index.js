5a59bc20921fa203aedc6b8856c8951a568b22b6
Pawel <pawelgilewicz2009@gmail.com>
2026-05-17 17:50:55 +0200

    another changes for web app

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const app = express();
 const port = 3000;
 const db = new pg.Client(
   {
-    "port":process.env.DB_PORT,
-    "host":process.env.DB_HOST,
-    "user":process.env.DB_USER,
-    "password":process.env.DB_PASSWORD,
-    "base":process.env.DB_BASE
+    port:process.env.DB_PORT,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_BASE
   }
 );
 db.connect();
@@ -26,23 +26,40 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+app.get("/",async (req, res) => {
+  
+  items = await db.query("SELECT * FROM items");
+ 
+  const data = items.rows;
   
   res.render("index.ejs", {
     listTitle: "Today",
-    listItems: items,
+    listItems: data,
   });
 });
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  // items.push({ title: item });
+  db.query("INSERT INTO items (title) VALUES($1) RETURNING *",[item])
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  const id = req.body.updatedItemId;
+  const changedTitle = req.body.updatedItemTitle;
+  
+  db.query("UPDATE items SET title=$1 WHERE id=$2",
+    [changedTitle,id]
+  );
+  res.redirect("/");
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", (req, res) => {
+  const id = req.body.deleteItemId;
+  db.query("DELETE from items WHERE id=$1",[id]);
+  res.redirect("/")
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
